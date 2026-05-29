a7aed8e611d2a0704d09c0ba19fede9fc79351b2
BADER_IN_4K <benbader1231@gmail.com>
2026-05-29 22:08:17 +0100

    mc

--- Web/lib/api.ts
+++ Web/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { clearAuthStorage } from "./auth"
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api"
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8083/api"
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
